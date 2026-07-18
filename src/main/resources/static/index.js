06ddc1e2e0a396c3585bee4ddbd8aa0eb2b8ae80
Ankit Singh <ankitsinghrb3@gmail.com>
2026-07-18 12:10:21 +0530

    Add user toggle functionality and update HTML structure for user display

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,28 @@
+var users = [
+    {
+        "name": "John Doe",
+        "gender": "Male",
+        "img":"john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender": "Female",
+        "img":"jane.png"
+    },
+]
+
+var id = 0;
+
+function toggleUser(){
+     id = (id + 1)%2
+
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
