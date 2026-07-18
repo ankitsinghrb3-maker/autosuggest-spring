d6a6ddce247622bc7e050e111fcc9f5b38a680c6
Ankit Singh <ankitsinghrb3@gmail.com>
2026-07-18 12:48:26 +0530

    Implement random user functionality and update button action in HTML

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -26,3 +26,32 @@ function toggleUser(){
     userGender.innerHTML = users[id].gender;
 
 }
+
+function randomUser(){
+    fetch('https://randomuser.me/api/')
+        .then(function(response){
+          
+            return response.json();
+        })
+        
+        .then(function(data){
+
+            var userData = data.results[0];
+
+         
+            var userImage = document.getElementById("user-image");
+            userImage.src = userData.picture.large;
+
+            var userName = document.getElementById("user-name");
+            userName.innerHTML = userData.name.first + " " +
+                                    userData.name.last;
+
+            
+            var userGender = document.getElementById("user-gender");
+            userGender.innerHTML = userData.gender;
+        })
+        
+        .catch(function(err){
+            console.log(err);
+        })
+}
